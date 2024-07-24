341def06e7060c8e48bbef4405a9dd6cca310c91
chappy <lumangyao112@gmail.com>
2024-07-24 23:53:33 +0800

    [feature] Create application pagination

--- src/app/(auth)/course/@CourseData/page.tsx
+++ src/app/(auth)/course/@CourseData/page.tsx
@@ -11,10 +11,26 @@ import { CourseType } from "../../../_lib/types/schema.type"
 import { formatDate } from "../../../_lib/utils/date.utils"
 
 const CourseData = async () => {
-  const data = await getCourses()
+  const data = await getCourses();
+  const totalPage = data.length / 8;
+  
+  console.log( totalPage > Math.floor(totalPage) ? Math.floor(totalPage) + 1 : totalPage);
+  return (
+  <div className=" w-full">
+   <table className="min-w-full text-left text-sm whitespace-nowrap">
+        <thead className="uppercase tracking-wider border-b border-b-white bg-white bg-opacity-80">
+          <tr>
+            <TableHeader>Course ID</TableHeader>
 
-  return data.map((val: CourseType, i: number) => {
+            <TableHeader>Course Name</TableHeader>
+            <TableHeader>Date Created</TableHeader>
+            <TableHeader>Action</TableHeader>
+          </tr>
+        </thead>
+  <tbody className=" w-full">
+    {data.data.map((val: CourseType, i: number) => {
     return (
+
       <TableRow key={i.toString()}>
         <TableData>{val.courseCode}</TableData>
         <TableData>{val.courseName}</TableData>
@@ -25,8 +41,15 @@ const CourseData = async () => {
           <a href={Routes.DASHBOARD}>Update</a>
         </TableData>
       </TableRow>
-    )
-  })
+    )})}
+  </tbody>
+      </table>
+    <div>
+    </div>
+   <p>{data.length}</p>
+   </div>
+  )
+ 
 }
 
 export default CourseData
--- src/app/(auth)/course/page.tsx
+++ src/app/(auth)/course/page.tsx
@@ -3,26 +3,18 @@ import { TableHeader } from "@/app/_lib/components"
 import CourseData from "./@CourseData/page"
 import CreateCourse from "./@CreateCourse/page"
 
-export default function Course() {
-  return (
+type PageParams =  { params: string, searchParams: { page: string } }
+
+export default function Course(params:PageParams) {
+ const {page} = params.searchParams;
+ 
+ console.log("PAGE",page);
+ 
+ return (
     <div className=" flex  flex-1  h-full flex-col">
       <CreateCourse />
       <div className=" h-10" />
-
-      <table className="min-w-full text-left text-sm whitespace-nowrap">
-        <thead className="uppercase tracking-wider border-b border-b-white bg-white bg-opacity-80">
-          <tr>
-            <TableHeader>Course ID</TableHeader>
-
-            <TableHeader>Course Name</TableHeader>
-            <TableHeader>Date Created</TableHeader>
-            <TableHeader>Action</TableHeader>
-          </tr>
-        </thead>
-        <tbody className=" w-full">
           <CourseData />
-        </tbody>
-      </table>
-    </div>
+      </div>
   )
 }
